fa8c83d710682a21626ab3f4d24f2fc76b10a954
vivek18f <kumarvivek9545@gmail.com>
2021-07-05 11:17:41 +0530

    to improve responsiveness used after in bubble sort and created a virtual environment

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,45 +1,57 @@
 from time import sleep
 
+def one(i, j, visObj):
+    for x in range(visObj.arrSize):
+        if x <= visObj.arrSize-i-1:
+            visObj.colArr[x] = "orange"
+        if x == j:
+            visObj.colArr[x] = "blue"
+        if x == j + 1:
+            visObj.colArr[x] = "magenta"
+        if x > visObj.arrSize - i - 1:
+            visObj.colArr[x] = visObj.themeColor
+    visObj.drawArr()
+
+def two(i, j, visObj):
+    for x in range(visObj.arrSize):
+        if x <= visObj.arrSize-i-1:
+            visObj.colArr[x] = "orange"
+        if x == j:
+            visObj.colArr[x] = visObj.bgColor
+        if x == j + 1:
+            visObj.colArr[x] = visObj.bgColor
+    visObj.drawArr()
+
+def three(i, j, visObj):
+    for x in range(visObj.arrSize):
+        if x <= visObj.arrSize-i-1:
+            visObj.colArr[x] = "orange"
+        if x == j:
+            visObj.colArr[x] = "magenta"
+        if x == j + 1:
+            visObj.colArr[x] = "blue"
+    visObj.drawArr()
+
 def bubbleSort(visObj):
     visObj.disableUI()
-
+    count = 0
     for i in range(visObj.arrSize- 1):
         for j in range(visObj.arrSize-1-i):
-
-            for x in range(visObj.arrSize):
-                if x <= visObj.arrSize-i-1:
-                    visObj.colArr[x] = "orange"
-                if x == j:
-                    visObj.colArr[x] = "blue"
-                if x == j + 1:
-                    visObj.colArr[x] = "magenta"
-                if x > visObj.arrSize - i - 1:
-                    visObj.colArr[x] = visObj.themeColor
-            visObj.drawArr()
-            sleep(visObj.delay)
+            
+            #call one
+            visObj.root.after(int(count*visObj.delay), one(i, j, visObj))
+            count += 1
 
             if visObj.arr[j] > visObj.arr[j+1]:
 
-                for x in range(visObj.arrSize):
-                    if x <= visObj.arrSize-i-1:
-                        visObj.colArr[x] = "orange"
-                    if x == j:
-                        visObj.colArr[x] = visObj.bgColor
-                    if x == j + 1:
-                        visObj.colArr[x] = visObj.bgColor
-                visObj.drawArr()
-                sleep(visObj.delay)
+                #call two
+                visObj.root.after(int(count*visObj.delay), two(i, j, visObj))
+                count+= 1
 
                 #swapping
                 visObj.arr[j], visObj.arr[j+1] = visObj.arr[j+1], visObj.arr[j]
 
-                for x in range(visObj.arrSize):
-                    if x <= visObj.arrSize-i-1:
-                        visObj.colArr[x] = "orange"
-                    if x == j:
-                        visObj.colArr[x] = "magenta"
-                    if x == j + 1:
-                        visObj.colArr[x] = "blue"
-                visObj.drawArr()
-                sleep(visObj.delay)
+                #call three
+                visObj.root.after(int(count*visObj.delay), three(i, j, visObj))
+                count += 1
 
